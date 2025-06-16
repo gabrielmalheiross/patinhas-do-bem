cecd4f89b18ceb0d2c4f9e3a07d5c14d3fb577c9
Gabriel Malheiros <gebrielmalheiros1515@gmail.com>
2025-06-15 21:40:15 -0300

    fix: trocar variaveis para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/patinhas-do-bem"
+  plugins: [react()]
 })
